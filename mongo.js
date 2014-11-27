d36e91b8b23520fd72e087a3b6a97a9c8dcea230
Alex Wakeman <alex@theworldmoves.me>
2014-11-27 15:02:45 +0000

    First commit
    Needs cleanup, no globals!

---
+++ mongo.js
@@ -0,0 +1,198 @@
+
+/*
+    Singleton class to ensure database access limited to one connection and instance
+
+ */
+var Niu = module.exports = function () {
+    "use strict";
+
+    return Niu.prototype.getInstance();
+};
+
+Niu.prototype = (function() {
+
+    // private variables (use get & set if required)
+    var _db, // maintain persistent reference to mongo DB
+        _mongoClient = require('mongodb').MongoClient,
+        q = require('q'),
+        _callback = function(err, data) {
+            if (err) throw err;
+            return data;
+        },
+        app = GLOBAL.app;
+
+    return {
+
+        connect: function(host) {
+
+            _mongoClient.connect(host, function(err, db) {
+
+                if (err) throw err;
+                _db = db;
+
+
+                if (app.get('env') === GLOBAL.config.PRODUCTION) {
+
+                    _db.authenticate(GLOBAL.config.MONGO_USER, GLOBAL.config.MONGO_PASS , function(err) {
+                        if(err) console.log("Unable to authenticate MongoDB!");
+                    });
+                }
+            });
+        },
+
+        disconnect: function() {
+            _db.close();
+        },
+
+        addEntry: function(collectionName, doc, callback) {
+
+            if (!callback) callback = _callback;
+
+            _db.collection(collectionName, function(err, collection) {
+
+                collection.insert(doc, {w:1}, callback);
+            });
+        },
+
+        findAll: function(collectionName, callback) {
+
+            _db.collection(collectionName, function(err, collection) {
+
+                collection.find().toArray(callback);
+            });
+        },
+
+        promiseFindAll: function(collectionName) {
+
+            var deferred = q.defer();
+
+            _db.collection(collectionName, function(err, collection) {
+
+                collection.find().toArray(function(err, data) {
+
+                    if (err) deferred.reject(err);
+                    else deferred.resolve(data);
+                });
+            });
+
+            return deferred.promise;
+        },
+
+        findById: function(collectionName, id, callback) { // callback(err, item)
+
+            if (!callback) {
+                console.error('Callback is required for findById.');
+                return false;
+            }
+
+            _db.collection(collectionName, function(err, collection) {
+
+                collection.findOne({ _id: _db.bson_serializer.ObjectID.createFromHexString(id) }, callback);
+            });
+        },
+
+        promiseFindById: function(collectionName, id) {
+
+            var deferred = q.defer();
+
+            _db.collection(collectionName, function(err, collection) {
+
+                collection.findOne({ _id: _db.bson_serializer.ObjectID.createFromHexString(id) }, function(err, data) {
+
+                    // finish promise process
+                    if (err) deferred.reject(err);
+                    else deferred.resolve(data);
+                });
+            });
+
+            return deferred.promise;
+        },
+
+        promiseFindOneByObject: function(collectionName, whereObj) {
+
+            // create promise object to return to caller
+            var deferred = q.defer();
+
+            _db.collection(collectionName, function(err, collection) {
+
+                collection.findOne(whereObj, function(err, data) {
+
+                    // finish promise process
+                    if (err) deferred.reject(err);
+                    else deferred.resolve(data);
+                });
+            });
+
+            return deferred.promise;
+        },
+
+        findOneByObject: function(collectionName, whereObj, callback) { // callback(err, item)
+
+            _db.collection(collectionName, function(err, collection) {
+
+                collection.findOne(whereObj, callback);
+            });
+        },
+
+        findAllByObject: function(collectionName, whereObj, callback) { // callback(err, item)
+
+            _db.collection(collectionName, function(err, collection) {
+
+                collection.find(whereObj).toArray(callback);
+            });
+        },
+
+        promiseFindAllByObject: function(collectionName, whereObj) {
+
+            // create promise object to return to caller
+            var deferred = q.defer();
+
+            _db.collection(collectionName, function(err, collection) {
+
+                collection.find(whereObj).toArray(function(err, data) {
+
+                    // finish promise process
+                    if (err) deferred.reject(err);
+                    else deferred.resolve(data);
+                });
+            });
+
+            return deferred.promise;
+        },
+
+        updateEntry: function(collectionName, id, doc, callback) {
+
+            // cannot update if _id property is present in document
+            if (doc._id) {
+                delete doc._id;
+            }
+
+            _db.collection(collectionName, function(err, collection) {
+
+                collection.update(
+                    { _id: _db.bson_serializer.ObjectID.createFromHexString(id) },
+                    { '$set': doc },
+                    callback
+                );
+            });
+
+            // return the ID here so it can be re-used in async operations
+            return id;
+        },
+
+        removeEntry: function(collectionName, id, callback) {
+
+            _db.collection(collectionName, function(err, collection) {
+                collection.remove({ _id: _db.bson_serializer.ObjectID.createFromHexString(id) }, callback)
+            });
+        },
+
+        getInstance: function() {
+
+            if (typeof Niu.prototype.instance === undefined) {
+                Niu.prototype.instance = new Niu();
+            }
+            return Niu.prototype.instance;
+        }
+    }
+})();
